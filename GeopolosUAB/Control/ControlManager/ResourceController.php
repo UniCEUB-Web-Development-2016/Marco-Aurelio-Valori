eb111b478868c025614030b46c4d2fd6d7f7d84b
Marco Aurélio Valori <bonovalori@hotmail.com>
2016-05-16 02:07:54 -0300

    Update ResourceController.php

--- GeopolosUAB/Control/ControlManager/ResourceController.php
+++ GeopolosUAB/Control/ControlManager/ResourceController.php
@@ -1,15 +1,30 @@
 <?php
 include_once "model/Request.php";
-include_once "control/UserController.php";
-include_once "control/PolosUAB_Controller.php";
+include_once "Control/ControlManager/UserController.php";
+include_once "Control/ControlManager/PolosUAB_Controller.php";
+include_once "Control/ControlManager/Courses_Controller.php";
 
 class ResourceController
 {
 	private $controlMap = 
 	[	"user" => "UserController",
-		"polosuab" => "PolosUAB_Controller"];
+		"polosuab" => "PolosUAB_Controller",
+		"course" => "Courses_Controller"];
+	
 	public function createResource($request)
 	{
 		return (new $this->controlMap[$request->get_resource()]())->register($request);
 	}
+	public function searchResource($request)
+	{
+		return (new $this->controlMap[$request->get_resource()]())->search($request);
+	}
+	public function deleteResource($request)
+	{
+		return (new $this->controlMap[$request->get_resource()]())->ReqDelete($request);
+	}
+	public function updateResource($request)
+	{
+		return (new $this->controlMap[$request->get_resource()]())->update($request);
+	}	
 }
