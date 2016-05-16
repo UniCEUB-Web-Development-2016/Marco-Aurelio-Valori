f4081762c0a50d1e3d59b6d15004d977063d0e2a
Marco Aurélio Valori <bonovalori@hotmail.com>
2016-05-16 02:13:58 -0300

    Update UserController.php

--- GeopolosUAB/Control/ControlManager/UserController.php
+++ GeopolosUAB/Control/ControlManager/UserController.php
@@ -1,11 +1,13 @@
 <?php
-include_once "model/Request.php";
-include_once "model/User.php";
+include_once "GeopolosUAB/Classes and Objects/Request.php";
+include_once "GeopolosUAB/Classes and Objects/User.php";
 class UserController
 {
 	public function register($request)
 	{
 		$params = $request->get_params();
+		if($this->isEmpty($params) == true)
+		{
 		$user = new User($params["name"],
 				 $params["last_name"],
 				 $params["email"],
@@ -14,10 +16,30 @@ class UserController
 				 $params["pass"]);
 		$db = new DatabaseConnector("localhost", "GeopolosUAB", "mysql", "", "root", "");
 		$conn = $db->getConnection();
+		return $conn->query($this->generateInsertQuery($courses));
+		} else {
+			return "There are empty fields!!!";
+		}
 		
-		
-	    return $this->generateInsertQuery($user);	
 	}
+	
+	private function isEmpty($params)
+	{
+		if($this->compare($params) == null)
+		{
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private function compare($params)
+	{
+		$paramsMap = ["name" => "", "type" => "", "year" => "", "students" => ""];
+		$result = array_diff_key($paramsMap, $params);
+		return $result;
+	}
+	
 	private function generateInsertQuery($user)
 	{
 		$query =  	"INSERT INTO User (id, name, status, situation, long, lat, uf, year) VALUES ('".$user->get_id()."','".
@@ -33,10 +55,16 @@ class UserController
 	public function search($request)
 	{
 		$params = $request->get_params();
+		if($this->isEmpty($params) == true)
+	{
 		$crit = $this->generateCriteria($params);
 		$result = "SELECT name, last_name, email, nickname, type FROM User WHERE ".$crit;
-		return $result;
+		return $result->fetchAll(PDO::FETCH_ASSOC);
+		}else {
+		return "There are empty fields!!!";
 	}
+	}
+	
 	private function generateCriteria($params) 
 	{
 		$criteria = "";
@@ -45,6 +73,16 @@ class UserController
 			$criteria = $criteria.$key." LIKE '%".$value."%' OR ";
 		}
 		return substr($criteria, 0, -4);	
+	}
+	
+	private function deleteCriteria($params) 
+	{
+		$criteria = "";
+		foreach($params as $key => $value)
+		{
+			$criteria = $criteria.$key." LIKE '".$value."' AND ";
+		}
+		return substr($criteria, 0, -5);
 	}
 	
 }
